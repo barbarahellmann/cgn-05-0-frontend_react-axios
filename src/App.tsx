7ef2d6478bb87fa0920f7e9308e4aa320ce3180f
barbarahellmann <babshellmann@web.de>
2024-04-19 15:38:19 +0200

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,65 @@
 import axios from "axios";
+import {useEffect, useState} from "react";
+import "./App.css"
+
+
+type Person = {
+  id: number,
+  email: string,
+  first_name: string,
+  last_name: string,
+  url: string,
+  avatar: string
+}
 
 function App() {
 
-  function fetchData() {
-    axios.get("https://regres.in/api/users").then((response) =>{
-      console.log(response.data)
+  const [data, setData] = useState<Person[]>([]);
+  const [page, setPage] = useState<number>(1)
+  const [name, setName] = useState<string>("")
+  const [job, setJob] = useState<string>("")
+
+  function updatePage(number: number) {
+    setPage(number)
+  }
+
+
+  useEffect(() => {
+    fetchData(page)
+  }, [page])
+
+
+
+  function fetchData(page: number) {
+    axios.get("https://reqres.in/api/users?page=" + page).then((response) => {
+      setData(response.data.data)
+    }).catch(error => {
+      console.log(error.message)
     })
   }
 
-  fetchData()
+  console.log(data)
+
+  function postData() {
+    axios.post("https://reqres.in/api/users", { name: name, job: job}).then(response => console.log(response))
+  }
 
   return (
-    <>
-        hello world
-    </>
+      <>
+        {data.length === 0 ? <h1>NO DATA</h1>: <ul>
+          {data.map((person: Person) => {
+            return <li key={person.id}>{person.first_name} {person.last_name}</li>
+          })}
+        </ul>}
+
+        <ul className={"pagination"}><li className={"item"} onClick={() => updatePage(1)}> 1</li><li className={"item"} onClick={() => updatePage(2)}>2</li></ul>
+        <label>Name</label>
+        <input type={"text"} onChange={(event) => setName(event.target.value)} value={name}/>
+        <label>Job</label>
+        <input type={"text"} onChange={(event) => setJob(event.target.value)} value={job}/>
+        <button onClick={postData}>Abschicken</button>
+
+      </>
   )
 }
 
